babbf74af6233f4d1b3cd0e56ecdaa6e8b6075b2
Michał Frontczak <77734214+mfrontczak@users.noreply.github.com>
2021-11-10 17:56:39 +0100

    Create zadanie3_szachownica.py

---
+++ materiały/lab4/zadanie3_szachownica.py
@@ -0,0 +1,13 @@
+
+# from collections import namedtuple
+# Figura = namedtuple('Figura', ['ranga', 'symbol'])
+
+class Figura:
+    def __init__(self, ranga, symbol):
+        self.ranga = ranga
+        self.symbol = symbol
+
+        
+# TBD
+class Szachownica:
+    pass
